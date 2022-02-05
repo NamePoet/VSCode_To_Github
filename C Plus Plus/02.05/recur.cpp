e2ed9c6a0c0d9032aac2e1269f5eabea80058af4
NamePoet <y181388072@163.com>
2022-02-06 00:29:37 +0800

    C++ recursion

--- C Plus Plus/02.05/recur.cpp
+++ C Plus Plus/02.05/recur.cpp
@@ -11,10 +11,10 @@ int main()
 void countdown(int n)
 {
     using namespace std;
-    cout << "Counting down ... " << n << endl;
+    cout << "Counting down ... " << n << " (n at " << &n << ")" << endl;
     if (n > 0)
         countdown(n-1);     // function calls itself
-    cout << n << ": Kaboom!\n";
+    cout << n << ": Kaboom!" << " (n at " << &n << ")" << endl;
 }
 /*
 
@@ -28,5 +28,21 @@ void countdown(int n)
     2: Kaboom!
     3: Kaboom!
     4: Kaboom!
+
+*/
+
+/*
+
+    Counting down ... 4 (n at 0x6ffe00)
+    Counting down ... 3 (n at 0x6ffdc0)
+    Counting down ... 2 (n at 0x6ffd80)
+    Counting down ... 1 (n at 0x6ffd40)
+    Counting down ... 0 (n at 0x6ffd00)
+    0: Kaboom! (n at 0x6ffd00)
+    1: Kaboom! (n at 0x6ffd40)
+    2: Kaboom! (n at 0x6ffd80)
+    3: Kaboom! (n at 0x6ffdc0)
+    4: Kaboom! (n at 0x6ffe00)
+
 
 */
