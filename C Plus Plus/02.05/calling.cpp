76aa328f98a4d04b3b87b6daf120be8a3c4b31f7
NamePoet <y181388072@163.com>
2022-02-05 12:00:42 +0800

    morning : function review

---
+++ C Plus Plus/02.05/calling.cpp
@@ -0,0 +1,28 @@
+// calling.cpp -- defining, prototyping, and calling a function
+#include <iostream>
+
+void simple(); // function prototype
+
+int main()
+{
+    using namespace std;
+    cout << "main() will call the simple() function:\n";
+    simple();    // function call
+        cout << "main() is finished with the simple() function.\n";
+    //cin.get();
+    return 0;
+}
+
+// function definition
+void simple()
+{
+    using namespace std;
+    cout << "I'm but a simple function.\n";
+}
+/*
+
+    main() will call the simple() function:
+    I'm but a simple function.
+    main() is finished with the simple() function.
+
+*/
